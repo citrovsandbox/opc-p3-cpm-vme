b5ce485c687481b08195dace87f4441e18712334
citrovsandbox <citrovsandbox@gmail.com>
2018-09-04 23:08:36 +0200

    Implémentation de la page de contact

---
+++ blog/contact/index.php
@@ -0,0 +1,45 @@
+<?php
+require("../../model/Chapitre.php");
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Jean Forteroche</title>
+    <link rel="stylesheet" href="../../lib/css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../src/css/common-blog.css">
+    <link rel="stylesheet" href="contact.css">
+    <script src="../../lib/js/jquery.js"></script>
+    <script src="../../lib/js/bootstrap.js"></script>
+    <script src="../../lib/js/bootstrap.bundle.js"></script>
+    <script src="contact.js"></script>
+</head>
+<body>
+<div id="pageContainer">
+    <!-- Partie sur le menu -->
+    <nav id="headerContainer">
+        <div id="menuContainer">
+            <div class="menu-item">
+                <a href="../livre" class="menu-text">Le livre</a></p>
+            </div>
+            <div class="menu-item">
+                <a href="#" class="menu-text">L'auteur</a></p>
+            </div>
+            <div class="menu-item">
+                <a href="../contact" class="menu-text">Le contact</a></p>
+            </div>
+        </div>
+    </nav>
+
+    <section id="banniereContainer">
+        <div id="contactContainer">
+            <div class="social"><a href="#"><i class="fab fa-facebook-square"></i> /jeanforteroche.auteur</a></div>
+            <div class="social"><a href="#"><i class="fab fa-twitter-square"></i> /jean.forteroche</a></div>
+            <div class="social"><a href="#"><i class="fab fa-instagram"></i> /j.forteroche</a></div>
+            <div class="social"><a href="#"><i class="fas fa-envelope-square"></i> contact@forteroche.fr</a></div>
+        </div>
+    </section>
+</div>
+</body>
+</html>
