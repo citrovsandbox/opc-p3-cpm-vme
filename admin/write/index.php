a8892192c2851b531cc9409612ab23f0386ae00f
citrovsandbox <citrovsandbox@gmail.com>
2018-09-07 00:32:11 +0200

    Implémentation du menu du backend

--- admin/write/index.php
+++ admin/write/index.php
@@ -12,15 +12,15 @@ protect();
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Dashboard</title>
+    <title>Ecrire un chapitre</title>
     <link rel="stylesheet" href="../../lib/css/bootstrap.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="../../src/css/common-admin.css">
-    <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="write.css">
     <script src="../../lib/js/jquery.js"></script>
     <script src="../../lib/js/bootstrap.js"></script>
     <script src="../../lib/js/bootstrap.bundle.js"></script>
-    <script src="dashboard.js"></script>
+    <script src="write.js"></script>
 </head>
 <body>
 <div id="pageContainer">
@@ -31,8 +31,8 @@ protect();
         </div>
         <div id="menuContainerBody">
             <div id="menuListContainer">
-                <div class="menu-item"><i class="fas fa-tachometer-alt"></i> <a href="#">Tableau de bord</a></div>
-                <div class="menu-item"><i class="fas fa-edit"></i> <a href="../write">Ecrire</a></div>
+                <div class="menu-item"><i class="fas fa-tachometer-alt"></i> <a href="../dashboard">Tableau de bord</a></div>
+                <div class="menu-item"><i class="fas fa-edit"></i> <a href="#">Ecrire</a></div>
                 <div class="menu-item"><i class="fas fa-clipboard-check"></i> <a href="#">Modérer</a></div>
                 <div class="menu-item"><i class="fas fa-sign-out-alt"></i> <a href="../lock.php">Déconnexion</a></div>
             </div>
